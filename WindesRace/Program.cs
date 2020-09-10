9aba539d2079f5d7af9d8870e163ced8ecb24464
KolmschateS <skolmschate@hotmail.nl>
2020-09-10 14:43:19 +0200

    First commit

---
+++ WindesRace/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WindesRace
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
