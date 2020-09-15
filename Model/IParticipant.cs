0bc672eae03db3f62e2a75f3d1cf5abc83ad8432
skolm <skolmschate@hotmail.nl>
2020-09-15 13:37:17 +0200

    2.1

---
+++ Model/IParticipant.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Model
+{
+    interface IParticipant
+    {
+        string Name { get; set; }
+        int Points { get; set; }
+        IEquipment Equipement { get; set; }
+        TeamColors TeamColor { get; set; }
+    }
+
+    enum TeamColors{
+        Red,
+        Green,
+        Blue,
+        Yellow,
+        Grey
+    }
+}
---
+++ Model/Track.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Model
+{
+    class Track
+    {
+        String name { get; set; }
+        LinkedList<Section> Sections { get; set; }
+    }
+}
