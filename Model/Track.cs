4da3e6ef025d84e568ece2e8df808c94ce9f6133
KolmschateS <skolmschate@hotmail.nl>
2020-10-09 09:31:32 +0200

    begin of visualization

--- Model/Track.cs
+++ Model/Track.cs
@@ -6,13 +6,31 @@ namespace Model
 {
     public class Track
     {
-        public String name { get; set; }
+        public String Name { get; set; }
         public LinkedList<Section> Sections { get; set; }
 
         public Track(string name, LinkedList<Section> sections)
         {
-            this.name = name;
+            this.Name = name;
             Sections = sections;
+        }
+        public Track(string name, SectionTypes[] _sectionTypes)
+        {
+            this.Name = name;
+            Sections = GenerateTrack(_sectionTypes);
+        }
+
+        public LinkedList<Section> GenerateTrack(SectionTypes[] _sectionTypes)
+        {
+            LinkedList<Section> LLSection = new LinkedList<Section>();
+            foreach (SectionTypes _sectionType in _sectionTypes)
+            {
+ /*Creates new section, then assigns that sectiontype to the section. The section gets added to the linkedlist.*/
+                Section section = new Section();
+                section.SectionType = _sectionType;
+                LLSection.AddLast(section);
+            }
+            return LLSection;
         }
     }
 }
---
+++ WindesRace/Visualize.cs
@@ -0,0 +1,64 @@
+﻿using Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WindesRace
+{
+    public static class Visualize
+    {
+
+        public static void Initialize()
+        {
+
+        }
+
+        #region graphics
+
+        private static string[] _finishHorizontal = {
+            "----",
+            "  # ",
+            "  # ",
+            "----" };
+
+        private static string[] _startGrid = {
+            "----",
+            "   ]",
+            " ]  ",
+            "----" };
+
+        private static string[] _TrackHorizontal = {
+            "----",
+            "    ",
+            "    ",
+            "----" };
+
+        private static string[] _TrackVertical = {
+            "|  |",
+            "|  |",
+            "|  |",
+            "|  |" };
+
+        private static string[] _TurnLeft = {
+            "--'\' ",
+            "   '\'",
+            "   |",
+            "'\'  |" };
+
+
+        #endregion
+
+        public static void DrawTrack(Track _track)
+        {
+            string[] _drawing = { "", "", "", "" };
+            foreach (Section _section in _track.Sections)
+            {
+
+            }
+            for(int i = 0; i < _drawing.Length; i++)
+            {
+                Console.WriteLine(_drawing[i]);
+            }
+        }
+    }
+}
